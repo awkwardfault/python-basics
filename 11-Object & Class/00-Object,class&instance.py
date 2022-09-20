aac9015efddb13e36898137e14ee9f9b4c9f1b35
awkwardfault <86266125+awkwardfault@users.noreply.github.com>
2022-09-20 22:39:52 +0600

    Add files via upload

---
+++ 11-Object & Class/00-Object,class&instance.py
@@ -0,0 +1,6 @@
+# object and instance are the same thing.
+# object is a part of a class
+# class is considered as the main design
+#  and a class can contain many objects and the objects will carry all the features of that class
+# and this features are called 'Data Attributes'
+# and the type of things that a class can do is called 'Method'
